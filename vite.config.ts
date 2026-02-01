6e02bb08d599411042f57ffbd7cd4323c1f3c6ec
Your Name <you@example.com>
2026-02-02 03:34:37 +0530

    Support Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,16 +3,16 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
     server: {
         host: "::",
         port: 8080,
     },
     plugins: [react()],
-    base: "/Modern-Personal-Portfolio/",
+    base: mode === 'production' && !process.env.VERCEL ? "/Modern-Personal-Portfolio/" : "/",
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
         },
     },
-});
+}));
